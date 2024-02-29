68b241dc3f43165d37d69804709e776d6df1964b
786arbaz <arbazabbasi.deoli@gmail.com>
2024-02-29 17:34:18 +0530

    Update mysql_dag.py

--- mysql_dag.py
+++ mysql_dag.py
@@ -6,12 +6,11 @@ from mysql_script import execute_mysql_query
 # Define your MySQL connection string ID
 mysql_conn_id = 'mysql_conn'
 
-
 # Define your DAG
 dag = DAG(
     'mysql_insert_dag',
     description='A DAG to insert values into MySQL table',
-    schedule_interval=None,
+    schedule_interval=None,  # Set your desired schedule or None for manual triggering
     start_date=datetime(2024, 2, 28),
     catchup=False
 )
@@ -20,6 +19,8 @@ dag = DAG(
 mysql_insert_task = PythonOperator(
     task_id='mysql_insert_task',
     python_callable=execute_mysql_query,
+    provide_context=True,  # Set to True if your function requires the context
+    op_args=[mysql_conn_id],  # Pass any additional arguments your function needs
     dag=dag,
     retries=3
 )
