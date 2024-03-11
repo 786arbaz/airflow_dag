c5c709a0095aee1f753d27b23564ed1dcacaf4af
786arbaz <arbazabbasi.deoli@gmail.com>
2024-03-11 15:29:12 +0530

    Update mysql_script.py

--- mysql_script.py
+++ mysql_script.py
@@ -12,7 +12,7 @@ def execute_mysql_query():
 
     # Insert values into the table
     query = "INSERT INTO testing (job_id, job_name) VALUES (%s, %s)"
-    values = (105, "gulshan")
+    values = (120, "shubham")
     cursor.executemany(query, values)  # Use executemany to insert multiple rows
 
     print('Values uploaded')
